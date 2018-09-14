396aae6555c9f7dfb84ee487be630b2af2b9dcda
akari-ru <akari_ru@outlook.de>
2018-09-14 05:32:11 +0200

    akari_ru #08: Completed Arc Commandline Parser, Added ProgramControlContext.

--- suzuha/Program.cs
+++ suzuha/Program.cs
@@ -11,6 +11,7 @@ using System.Resources;
 using suzuha.moth;
 using suzuha.moth.FileTesting;
 using suzuha.moth.Threading;
+using suzuha.arc;
 
 namespace suzuha
 {
@@ -19,19 +20,35 @@ namespace suzuha
         static string programmName = "suzuha";
         static string programmVersion = "v 0.1";
 
+        static class ProgramControlContext
+        {
+            static bool threading = false;
+            static bool game = false;
+            static bool fileTesting = false;
+            static bool kara = false;
+
+            static string filePath = null;
+
+            public static bool FileTesting { get => fileTesting; set => fileTesting = value; }
+            public static bool Threading { get => threading; set => threading = value; }
+            public static bool Game { get => game; set => game = value; }
+            public static bool Kara { get => kara; set => kara = value; }
+
+            public static string FilePath { get => filePath; set => filePath = value; }
+        }
+
         static void Main(string[] args)
         {
             PrintStartupMessage();
-            // ThradingCodeMod();
-            // StreamingCodeMode();
-
-            PrintCultureInfo();
-
-            var rm = new ResourceManager("suzuha.strings.Resources", typeof(Program).Assembly);
-            Console.WriteLine(rm.GetString("msgIntro"));
-
-            Console.WriteLine("Nothing to do at the moment.");
-
+            SetUpProgramControlContext(args);
+            if (ProgramControlContext.Threading)
+                ThradingCodeMod();
+            if (ProgramControlContext.FileTesting)
+                FileTestingCodeMod();
+            if (ProgramControlContext.Game)
+                GameCodeMod();
+            if (ProgramControlContext.Kara)
+                KaraCodeMod();
             ProgammTerminatedPrompt();
         }
 
@@ -40,7 +57,9 @@ namespace suzuha
 
         static void PrintUsage()
         {
-            throw new NotImplementedException();
+            var res = new ResourceManager("suzuha.strings.Resources", typeof(Program).Assembly);
+            var usageString = res.GetString("usage");
+            Console.WriteLine(usageString);
         }
 
         static void PrintHelp()
@@ -69,11 +88,88 @@ namespace suzuha
         }
 
 
+        /* Ground Tasks */
+
+        static void SetUpProgramControlContext(string[] args)
+        {
+            Console.WriteLine("Arc parsing..");
+            foreach (var arg in args)
+            {
+                Console.Write(arg + " ");
+            }
+            Console.WriteLine();
+            var arc = new Arc(args, "f:tgk", "[file]:[threading][game][kara][file]");
+            List<Tuple<string, string>> parsed;
+            try
+            {
+                parsed = arc.Parse();
+            }
+            catch (ArcParsingException e)
+            {
+                Console.WriteLine(e.Message);
+                PrintUsage();
+                ProgammTerminatedPrompt();
+                return;
+            }
+
+            // todo: Remove Console Output
+            //-----------------------------
+            foreach (var tupel in parsed)
+            {
+                Console.WriteLine(tupel);
+            }
+            //-----------------------------
+
+            foreach (var tupel in parsed)
+            {
+                switch (tupel.Item1)
+                {
+                    case "f":
+                    case "file":
+                        if (!ProgramControlContext.FileTesting)
+                        {
+                            ProgramControlContext.FileTesting = true;
+                            ProgramControlContext.FilePath = tupel.Item2;
+                        }
+                        else
+                        {
+                            throw new Exception("File flag override prohibited.");
+                        }
+                        break;
+                    case "g":
+                    case "game":
+                        ProgramControlContext.Game = true;
+                        break;
+                    case "t":
+                    case "threading":
+                        ProgramControlContext.Threading = true;
+                        break;
+                    case "k":
+                    case "kara":
+                        ProgramControlContext.Kara = true;
+                        break;
+                    default:
+                        throw new Exception("Invalid Flag Init.");
+                }
+            }
+        }
+        
+        
         /* Code Modules */
 
         static void FileTestingCodeMod()     // unused ..
         {
             FileTesting.Testing();
+        }
+
+        static void GameCodeMod()
+        {
+            
+        }
+
+        static void KaraCodeMod()
+        {
+
         }
 
         static void ThradingCodeMod()
--- suzuha/arc/Arc.cs
+++ suzuha/arc/Arc.cs
@@ -7,49 +7,212 @@ using System.Threading.Tasks;
 
 namespace suzuha.arc
 {
-    // simple command line parser
-    class Arc : IEnumerable<Tuple<string, string>>
+
+    #region Custom Exceptions
+
+    public class ArcParsingException : Exception
+    {
+
+        public ArcParsingException() { }
+
+        public ArcParsingException(string message)
+            : base(message)
+        {
+
+        }
+
+        public ArcParsingException(string message, Exception inner)
+            : base(message, inner)
+        {
+
+        }
+
+    }
+
+    #endregion
+
+    // simple command line parser   WARNING: bugs may appear :/ , do not use until complete validation and testing of this class
+
+    // flags foramt: f.e. "f:hvq"
+    // if a flag ist followed by `:', u can specify an argument behind the flag
+    // it ist further possible to give an alternativ long name: "[file]", wich is invocable by using two `-'
+
+    public class Arc
     {
         private string[] Args { get; }
         private string Flags { get; }
+        private string LongformFlags { get; }
+        private char Seperator { get => ' '; }
+        private char FlagSign { get => '-'; }
+        private char ArgSign { get => ':'; }
 
         public Arc(string[] args, string flags)
         {
             if (args == null || flags == null) throw new ArgumentNullException();
             Args = args;
+            if (flags.Contains("[") || flags.Contains("]")) throw new ArgumentException("[ and ] ar not allowed for short form flag specification.");
             Flags = flags;
         }
 
-        public IEnumerator<Tuple<string, string>> GetEnumerator()
+        public Arc(string[] args, string flags, string longformFlags)
+            : this(args, flags)
         {
-            throw new NotImplementedException();
+            if (longformFlags == null) throw new ArgumentNullException();
+            LongformFlags = longformFlags;
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
+
+        private bool CheckForFlag(char flag)
         {
-            throw new NotImplementedException();
+            if (Flags.Contains(flag) && flag != ArgSign)
+                return true;
+            return false;
         }
 
-        private class ArcEnumerator : IEnumerator<Tuple<string, string>>
+        private bool CheckForAltFlag(string flag)
         {
-            public Tuple<string, string> Current => throw new NotImplementedException();
-
-            object IEnumerator.Current => throw new NotImplementedException();
-
-            public void Dispose()
-            {
-                throw new NotImplementedException();
-            }
-
-            public bool MoveNext()
-            {
-                throw new NotImplementedException();
-            }
-
-            public void Reset()
-            {
-                throw new NotImplementedException();
-            }
+            if (LongformFlags.Contains("[" + flag + "]"))
+                return true;
+            return false;
         }
+
+        private bool HasFlagArgument(char flag)
+        {
+            var p = Flags.IndexOf(flag);
+            if (!(p == Flags.Length-1))
+            {
+                if (Flags[p + 1] == ArgSign)
+                    return true;
+            }
+            return false;
+        }
+
+        private bool HasAltFlagArgument(string flag)
+        {
+            var p = LongformFlags.IndexOf(flag);
+            p += flag.Length;
+            if (!(p == LongformFlags.Length - 1))
+            {
+                if (LongformFlags[p + 1] == ArgSign)
+                    return true;
+            }
+            return false;
+        }
+
+        public List<Tuple<string, string>> Parse()
+        {
+            var echo = new List<Tuple<string, string>>();
+
+            // The parsing begins here ..
+            int p = 0;
+            ArcParseState state = ArcParseState.FLAG_CHECK;
+
+            string currentFlag = null;
+
+            foreach (var arg in Args)
+            {
+                if (state == ArcParseState.FLAG_CHECK)
+                {
+                    p = 0;
+                    if (arg[p] != FlagSign)
+                    {
+                        continue;
+                    }
+                    if (arg.Length == 1)
+                    {
+                        if (!CheckForFlag(arg[p])) throw new ArcParsingException("Invalid flag, empty flag not specified");
+                        echo.Add(new Tuple<string, string>("" + FlagSign, null));
+                        continue;
+                    }
+                    p += 1;
+                    if (arg[p] == FlagSign)
+                    {
+                        state = ArcParseState.ALT_NAME;
+                        p += 1;
+                    }
+                    else
+                    {
+                        state = ArcParseState.MULTI_FLAG;
+                    }
+
+                }
+                    
+                if (state == ArcParseState.ALT_NAME)
+                {
+                    if (arg.Length == 2)
+                    {
+                    // todo: specify Flags field format for -- empty long flag
+                        echo.Add(new Tuple<string, string>("" + FlagSign + FlagSign, null));
+                        state = ArcParseState.FLAG_CHECK;
+                        continue;
+                    }
+
+                    currentFlag = arg.Substring(p);
+                    if (!CheckForAltFlag(currentFlag)) throw new ArcParsingException("Invalid Flag (alternative long form). Flag is " + currentFlag + ".");
+                    if (!HasAltFlagArgument(currentFlag))
+                    {
+                        echo.Add(new Tuple<string, string>(currentFlag, null));
+                        currentFlag = null;
+                        state = ArcParseState.FLAG_CHECK;
+                    }
+                    else
+                    {
+                        state = ArcParseState.ARG_TOKEN;
+                    }
+                    
+                    continue;
+                }
+
+                if (state == ArcParseState.ARG_TOKEN)
+                {
+                    if (arg[p] == FlagSign) throw new ArcParsingException("Expected argument token, got new flag section instead.");
+                    echo.Add(new Tuple<string, string>(currentFlag, arg));
+                    currentFlag = null;
+                    state = ArcParseState.FLAG_CHECK;
+                    continue;
+                }
+
+                if (state == ArcParseState.MULTI_FLAG)
+                {
+                    while (p < arg.Length)
+                    {
+                        var flag = arg[p];
+                        if (!CheckForFlag(flag)) throw new ArcParsingException("Invalid Flag. Flag is " + flag + ".");
+                        if (HasFlagArgument(flag))
+                        {
+                            if (p != arg.Length - 1) throw new ArcParsingException("Flags with argument not at the end of an multiflag. Flag is " + flag + ".");
+                            currentFlag = flag.ToString();
+                            state = ArcParseState.ARG_TOKEN;
+                            break;
+                        }
+                        else
+                        {
+                            echo.Add(new Tuple<string, string>("" + flag, null));
+                        }
+                        p += 1;
+                    }
+                    if (state == ArcParseState.MULTI_FLAG)
+                        state = ArcParseState.FLAG_CHECK;
+                    continue;
+                }
+            }
+            if (state == ArcParseState.ARG_TOKEN) throw new ArcParsingException("Argument expected for flag " + currentFlag + ".");
+            return echo;
+        }
+        
+        #region State Enum
+
+        enum ArcParseState
+        {
+            BASE = 0,
+            FLAG_CHECK,
+            ALT_NAME,
+            MULTI_FLAG,
+            ARG_TOKEN,
+            END
+        }
+
+        #endregion
+
     }
 }
